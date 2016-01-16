c98eaa78057f23ee1460d2ea324ba9243fdfe63f
Wil Gieseler <wil@wilgieseler.com>
2016-01-15 23:19:29 -0800

    locations

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,13 @@
 class ItemsController < ApplicationController
 
-  def locations
+  def location
     @location_name = params[:name]
     @items = Item.where(location_name: @location_name)
     @table_show_categories = true
+  end
+
+  def locations
+    @location_names = Item.uniq.pluck(:location_name)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :categories
   resources :items
 
-  get 'locations/:name/items' => 'items#locations', as: :location
+  get 'locations/:name/items' => 'items#location', as: :location
+  get 'locations' => 'items#locations', as: :locations
 
   root "categories#show", id: 1
 
